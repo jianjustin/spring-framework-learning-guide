d3213d38b59f7039f355b7e012689662600078f6
jian <janine.jian.chen@gmail.com>
2019-10-17 22:09:43 +0800

    add module:spring-boot-guide-datasource

---
+++ spring-boot-guide-datasource/src/main/java/org/janine/jian/DataSourceApp.java
@@ -0,0 +1,32 @@
+package org.janine.jian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+@RestController
+public class DataSourceApp {
+
+
+    @RequestMapping("/")
+    public String home(){
+        return "the way to config custom datasource";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataSourceApp.class,args);
+    }
+
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public DataSource dataSource(){
+        return DataSourceBuilder.create().build();
+    }
+}
