3b54724dc25198675f6fd6d6599f5e25f15e073a
jian <janine.jian.chen@gmail.com>
2019-11-11 09:45:38 +0800

    del spring-boot-security-jwt project

--- spring-boot-security-jwt/src/main/java/org/janine/jian/repository/RandomCityRepository.java
+++
@@ -1,7 +0,0 @@
-package org.janine.jian.repository;
-
-import org.janine.jian.domain.RandomCity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RandomCityRepository extends CrudRepository<RandomCity,Long> {
-}
--- spring-boot-security-jwt/src/main/java/org/janine/jian/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package org.janine.jian.repository;
-
-import org.janine.jian.domain.Role;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RoleRepository extends CrudRepository<Role,Long> {
-}
