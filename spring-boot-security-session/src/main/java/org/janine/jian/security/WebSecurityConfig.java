5d2a7f0f025bc08f0aefc92ddb43d5a1d0966d85
jian <janine.jian.chen@gmail.com>
2019-11-15 15:02:46 +0800

    motify spring security modules

---
+++ spring-boot-security-session/src/main/java/org/janine/jian/security/WebSecurityConfig.java
@@ -0,0 +1,7 @@
+package org.janine.jian.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class WebSecurityConfig {
+}
