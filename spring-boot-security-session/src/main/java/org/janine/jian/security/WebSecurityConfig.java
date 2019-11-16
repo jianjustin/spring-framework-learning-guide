f8c57da2ebedfbc83eee8b572f20772b18283275
jian <janine.jian.chen@gmail.com>
2019-11-16 16:39:54 +0800

    simple form login security solution

--- spring-boot-security-session/src/main/java/org/janine/jian/security/WebSecurityConfig.java
+++ spring-boot-security-session/src/main/java/org/janine/jian/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package org.janine.jian.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
@@ -10,9 +11,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/login*").permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .csrf().disable()
                 .formLogin()
+                .loginProcessingUrl("/login")//重写登录路径
+                .loginPage("/login.html")//重写登录页面
                 .and()
                 .httpBasic();//WebSecurityConfigurerAdapter默认配置
     }
