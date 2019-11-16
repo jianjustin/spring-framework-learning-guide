6a465b12107d5e9eecd32cd876a620a389204c5b
jian <janine.jian.chen@gmail.com>
2019-11-16 17:06:35 +0800

    custom UsernamePasswordAuthenticationFilter

--- spring-boot-security-session/src/main/java/org/janine/jian/security/WebSecurityConfig.java
+++ spring-boot-security-session/src/main/java/org/janine/jian/security/WebSecurityConfig.java
@@ -1,6 +1,9 @@
 package org.janine.jian.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -10,7 +13,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.addFilterAt(new MyUsernamePasswordAuthenticationFilter(authenticationManagerBean()),UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
                 .antMatchers("/login*").permitAll()
                 .anyRequest().authenticated()
                 .and()
@@ -21,4 +25,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .and()
                 .httpBasic();//WebSecurityConfigurerAdapter默认配置
     }
+
 }
