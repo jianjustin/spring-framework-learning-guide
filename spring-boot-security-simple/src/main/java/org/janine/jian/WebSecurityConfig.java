f8b055a0a9ef86e2fc50a33f6a0455b72497d17f
jian <janine.jian.chen@gmail.com>
2019-11-16 11:12:07 +0800

    delete not used import

--- spring-boot-security-simple/src/main/java/org/janine/jian/WebSecurityConfig.java
+++ spring-boot-security-simple/src/main/java/org/janine/jian/WebSecurityConfig.java
@@ -1,15 +1,8 @@
 package org.janine.jian;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
