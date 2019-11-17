efcc54164ba644cb58cf61ee613f3b496884a145
jian <janine.jian.chen@gmail.com>
2019-11-17 10:58:25 +0800

    add default logout method

--- spring-boot-security-session/src/main/java/org/janine/jian/security/WebSecurityConfig.java
+++ spring-boot-security-session/src/main/java/org/janine/jian/security/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,6 +24,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .formLogin()
                 .loginProcessingUrl("/login")//重写登录路径
                 .loginPage("/login.html")//重写登录页面
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login.html")
                 .and()
                 .httpBasic();//WebSecurityConfigurerAdapter默认配置
     }
