aaf43e447372a29164237d1a2277d87cc6267396
jian <janine.jian.chen@gmail.com>
2019-11-25 14:47:34 +0800

    add module spring-boot-security-oauth-github-client

---
+++ spring-boot-security-oauth-github-client/src/main/java/org/janine/jian/security/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package org.janine.jian.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login.html")
+                .permitAll()
+                .and()
+                .oauth2Login();
+    }
+}
