a71ea8a5c7540943721254322e86c7ea39ee4ad8
jian <janine.jian.chen@gmail.com>
2019-12-02 13:31:59 +0800

    clean code - spring-boot-security-oauth-authorization-server

--- spring-boot-security-oauth-authorization-server/src/main/java/org/janine/jian/security/MyAuthorizationServerConfig.java
+++ spring-boot-security-oauth-authorization-server/src/main/java/org/janine/jian/security/MyAuthorizationServerConfig.java
@@ -1,25 +1,22 @@
 package org.janine.jian.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableAuthorizationServer
 public class MyAuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
     @Autowired
     public DataSource dataSource;
@@ -28,13 +25,13 @@ public class MyAuthorizationServerConfig extends AuthorizationServerConfigurerAd
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.authenticationManager(authenticationManager)
-            .tokenStore(tokenStore())
-            .accessTokenConverter(jwtAccessTokenConverter());
+        endpoints.authenticationManager(authenticationManager);
+            //.tokenStore(tokenStore())
+            //.accessTokenConverter(jwtAccessTokenConverter());
     }
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.withClientDetails(clientDetails());
+        clients.jdbc(dataSource);
     }
 
     @Override
@@ -47,12 +44,6 @@ public class MyAuthorizationServerConfig extends AuthorizationServerConfigurerAd
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
         converter.setSigningKey("oauth2");
         return converter;
-    }
-
-    @Bean
-    public ClientDetailsService clientDetails() {
-        ClientDetailsService clientDetailsService = new JdbcClientDetailsService(dataSource);
-        return clientDetailsService;
     }
 
     @Bean
