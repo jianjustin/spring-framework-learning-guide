2ca9127d9354eedcd61fe3d69a8b27f33a51a96f
jian <janine.jian.chen@gmail.com>
2020-02-19 17:29:41 +0800

    swagger config add securitySchemes

--- spring-boot-security-jwt/src/main/java/org/janine/jian/config/SwaggerConfig.java
+++ spring-boot-security-jwt/src/main/java/org/janine/jian/config/SwaggerConfig.java
@@ -1,5 +1,9 @@
 package org.janine.jian.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,8 +11,12 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -23,6 +31,9 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("org.janine.jian.controller"))
                 .paths(PathSelectors.any())
                 .build()
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts())
+                //.securitySchemes(new ArrayList<>(Arrays.asList(new ApiKey("Bearer %token", "Authorization", "Header"))))
                 .tags(new Tag("auth", "用户认证"))
                 .tags(new Tag("authority", "授权模块"))
                 .tags(new Tag("role", "角色模块"))
@@ -31,8 +42,26 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("基于JWT的RESTful API")
+                .title("基于JWT构建RESTful API")
                 .build();
     }
-
+    
+	private List<ApiKey> securitySchemes() {
+		return new ArrayList<>(Arrays.asList(new ApiKey("Authorization", "Authorization", "header")));
+	}
+	
+	private List<SecurityContext> securityContexts() {
+		return new ArrayList<>(Arrays.asList(
+		    SecurityContext.builder()
+		            .securityReferences(defaultAuth())
+		            .forPaths(PathSelectors.regex("^(?!auth).*$"))
+		            .build())
+		);
+	}
+	List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return new ArrayList<>(Arrays.asList(new SecurityReference("Authorization", authorizationScopes)));
+	}
 }
